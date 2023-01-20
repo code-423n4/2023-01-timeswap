1e2fc957cb9219d41186686a15d1b22d0b5b7a67
Heemank Verma <heemankv@gmail.com>
2023-01-20 17:10:54 +0530

    Addon: added coverage

--- packages/v2-option/test/TimeswapV2OptionFactory.t.sol
+++ packages/v2-option/test/TimeswapV2OptionFactory.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "../src/TimeswapV2OptionFactory.sol";
 import "../src/interfaces/ITimeswapV2Option.sol";
 
+
 contract TimeswapV2OptionFactoryTest is Test {
     TimeswapV2OptionFactory factory;
 
@@ -20,5 +21,17 @@ contract TimeswapV2OptionFactoryTest is Test {
         assertEq(token1, ITimeswapV2Option(opPair).token1());
         assertEq(opPair, factory.get(token0, token1));
         assertEq(address(factory), ITimeswapV2Option(opPair).optionFactory());
+    }
+
+    function testOptionFactoryGet(address token0, address token1) public {
+        vm.assume(token1 > token0);
+        vm.assume(token1 != address(0) && token0 != address(0));
+        address opPair = factory.create(token0, token1);
+        assertEq(opPair, factory.get(token0, token1));
+    }
+
+    function testOptionFactoryNumberOfPairs() public {
+        console.log(factory.numberOfPairs());
+        assertEq(0, factory.numberOfPairs());
     }
 }
